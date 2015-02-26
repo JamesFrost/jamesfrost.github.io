d02a1890c4dec511bd3e2869a3e15ad9be572cb2
James Frost <hello@jamesfrost.me>
2015-02-26 21:11:27 +0000

    Updated JS + CV

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){ 
 	$("#loading").hide();
 	$("#main").fadeIn("fast");	
+
+	window.addEventListener('scroll',sliderMove, false);
+
 });
 
 $(document).on("click", "a [target!='_blank']", function () {
@@ -8,7 +11,7 @@ $(document).on("click", "a [target!='_blank']", function () {
 	var newUrl = $(this).attr("href");
 
 	if (!newUrl || newUrl[0] === "#") {
-		location.hash = newUrl;
+		// location.hash = newUrl;
 		return;
 	}
 
@@ -18,3 +21,15 @@ $(document).on("click", "a [target!='_blank']", function () {
 
 	return false;
 });
+
+function sliderMove() {
+	var scrollTop = $(window).scrollTop() ;
+
+	var x=document.getElementById('slider');
+
+	var main = document.getElementById('main').offsetHeight;
+	var documentHeight = $(document).height();	
+
+	slider.style.width = (($(window).scrollTop() / ($(document).height() - $(window).height()))*$(document).width()) + 'px';			
+
+}
