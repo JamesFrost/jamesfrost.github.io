8cf7108e313b3b422d3ac1563afc136e5fd75501
James Frost <hello@jamesfrost.me>
2015-02-27 12:10:26 +0000

    slider only included on CV page

--- js/functions.js
+++ js/functions.js
@@ -2,8 +2,6 @@ $(document).ready(function(){
 	$("#loading").hide();
 	$("#main").fadeIn("fast");	
 
-	window.addEventListener('scroll',sliderMove, false);
-
 });
 
 $(document).on("click", "a [target!='_blank']", function () {
@@ -21,15 +19,3 @@ $(document).on("click", "a [target!='_blank']", function () {
 
 	return false;
 });
-
-function sliderMove() {
-	var scrollTop = $(window).scrollTop() ;
-
-	var x=document.getElementById('slider');
-
-	var main = document.getElementById('main').offsetHeight;
-	var documentHeight = $(document).height();	
-
-	slider.style.width = (($(window).scrollTop() / ($(document).height() - $(window).height()))*$(document).width()) + 'px';			
-
-}
